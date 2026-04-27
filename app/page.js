ec668ceb119afd0b11b46c5ab5de781a22363e72
Xerdiscalot <161652032+Xerdiscalot@users.noreply.github.com>
2026-04-27 16:36:29 -0500

    Update page.js

--- app/page.js
+++ app/page.js
@@ -1,8 +1,9 @@
 export default function Home() {
+  const affiliateTag = process.env.NEXT_PUBLIC_AFFILIATE_TAG;
+
   return (
     <main style={{ padding: 40 }}>
       <h1>It Works</h1>
     </main>
   );
 }
-const affiliateTag = process.env.NEXT_PUBLIC_AFFILIATE_TAG;
